3a3eb8d73f3a93656cb376f338929a88e77b5c49
jomseni <108383921+jomseni@users.noreply.github.com>
2022-09-15 17:58:12 +0900

    0915 스프링 웹 개발 기초
    세 가지 중  두 번째인 MVC와 템플릿엔진
    
    정적인것이 아닌 동적인 페이지를 프로그래밍한다!

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -11,5 +12,10 @@ public class HelloController {
     public String hello(Model model){
         model.addAttribute("data","hello!");
         return "hello";
+    }
+    @GetMapping("hello-mvc") //url 주소에 치는 부분이라고 생각해도 된다 ex) localhost:8080/hello-mvc
+    public String helloMvc(@RequestParam ("name") String name, Model model){   //RequestParam을 통해 웹에서 파라미터를 받아서 적용한다!
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
