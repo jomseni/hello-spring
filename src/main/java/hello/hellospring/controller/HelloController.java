83c3eea8e5ac34745cec7f8fc3ce016aa9a54cf5
jomseni <108383921+jomseni@users.noreply.github.com>
2022-09-20 16:20:10 +0900

    09 20 스프링 웹 개발 기초 세 가지
    객체는 - json 형태로 반환한다
    
    정적 컨텐츠  = 파일을 그대로 내려준다
    MVC와 템플릿엔진 = 템플릿엔진을 모델뷰 컨트롤러 방식으로 쪼개서 뷰를 템블릿엔진  html 형태로 반환해 클라이언트에 전달해준다
    API = 객체를 반환한다(json형태)! 뷰 없이 http response방식으로 반환한다. ex) {'name'} = spring!!!!!!!의 방식

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -14,8 +15,39 @@ public class HelloController {
         return "hello";
     }
     @GetMapping("hello-mvc") //url 주소에 치는 부분이라고 생각해도 된다 ex) localhost:8080/hello-mvc
-    public String helloMvc(@RequestParam ("name") String name, Model model){   //RequestParam을 통해 웹에서 파라미터를 받아서 적용한다!
+    public String helloMvc(@RequestParam ("name") String name, Model model){   //RequestParam을 통해 웹에서 파라미터를 받아서 적용한다!(동적)
         model.addAttribute("name", name);
         return "hello-template";
+    }
+    @GetMapping("hello-string")
+    @ResponseBody  // http의 body부에 return 값의 데이터를 직접 넣겠다는 뜻이다!
+    public String helloString(@RequestParam("name")String name){
+        return "hello " + name; //만약  위의 String 파라미터에 sprting 을 넣는다면 hello spring 이 나올 것이다!!
+    }
+
+//    데이터를 내놔! 할 때 사용한다!
+    @GetMapping("hello-api")
+    @ResponseBody
+
+
+    //json 방식 : 출력결과
+    //최근엔 다 json 방식 사용한다
+   public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello; //객체를 return , 디폴트 값이 기본적으로 json방식으로 리턴한다!
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName(){ //꺼낼때는 getname
+
+            return name;
+        }
+
+        public void setName(String name){ //넣을때는 setname
+            this.name = name;
+        }
     }
 }
